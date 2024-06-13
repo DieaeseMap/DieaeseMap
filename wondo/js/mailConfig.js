ff2d1898cdc2b141b1adafafe01e311e016ba344
one-west <wjdgkstj112@naver.com>
2024-06-13 18:15:34 +0900

    update

--- wondo/js/mailConfig.js
+++ wondo/js/mailConfig.js
@@ -11,7 +11,6 @@ function mailConfig(fromEmail, subject, message) {
   const transporter = nodemailer.createTransport({
     host: "smtp.gmail.com",
     port: 587, // 이메일 서비스의 포트 번호 (ex: 25, 587, 465, 2525)
-    secure: false,
     auth: {
       user: process.env.EMAIL, // 환경변수에서 이메일 주소를 가져옴
       pass: process.env.PASSWORD, // 환경변수에서 애플리케이션 비밀번호를 가져옴
@@ -20,8 +19,8 @@ function mailConfig(fromEmail, subject, message) {
 
   // 메일 옵션 설정
   const mailOptions = {
-    from: fromEmail, // 작성자
-    to: process.env.EMAIL, // 수신자
+    from: process.env.EMAIL, // 작성자
+    to: fromEmail, // 수신자
     subject: subject, // 메일 제목
     text: message, // 메일 내용
   };
